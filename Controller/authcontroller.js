ce91ce1b7cad993744044934a17a3911e34552d1
HaileA2 <hailea536@gmail.com>
2025-07-18 19:07:09 +0200

    all modules are modified proprely

--- Controller/authcontroller.js
+++ Controller/authcontroller.js
@@ -1,10 +1,21 @@
+const jwt = require('jsonwebtoken');
 const User=require('./../models/usermodel')
+const { ServerClosedEvent } = require("mongodb")
  exports.signup=async(req,res,next)=>{
-    const newuser=await User.create(req.body)
+    const newuser=await User.create({
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password,
+        passwordconfirm:req.body.passwordconfirm
+    })
+    const Token=jwt.sign({id:newuser._id},process.env.JWTSECRET,{expiresIn:process.env.JWTEXPIRESIN})
 
     res.status(201).json({
         status:"success",
         code:201,
+
+        Token,
         data:{newuser}
+
     })
  }
